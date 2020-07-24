7fdf9e6a27b0e986f4e37710c27249e387cb8749
Shruti3004 <mail2shruti.ag@gmail.com>
2020-07-24 08:09:11 +0530

    eateria

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -32,7 +32,7 @@ function Details() {
                     <hr className="py-0 my-0"/>
                     <h5 className="text-purple text-center mt-2">
                         <strong>
-                            Price: &#8377;{price}
+                            Price: &#8377;&nbsp;{price}
                         </strong>
                     </h5>
                     <hr/>
@@ -71,11 +71,12 @@ const DetailImage = styled.div`
     .card{
         border-color: transparent;
         transition: all 0.4s linear;
+        border: 0.04rem solid rgba(0,0,0,0.2);
+        box-shadow: 3px 3px 10px 0px rgba(0,0,0,0.4);
     }
     &:hover{
         .card{
-            border: 0.04rem solid rgba(0,0,0,0.2);
-            box-shadow: 3px 3px 10px 0px rgba(0,0,0,0.4);
+            box-shadow: 3px 3px 15px 0px rgba(0,0,0,0.4);
         }
     }
     .img-container{
